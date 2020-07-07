e8a88c7d2b066fa256424225f114f96c2a2d01fa
JumarySanchez <sanchezjumary@gmail.com>
2020-07-07 12:18:05 -0400

    nice the progress shows

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds= function(num){
+let newArray= []
+
+for(const odd of num){
+  if (odd % 2 ===1){
+    newArray.push(odd)
+  }
+}return newArray
+}
+
+
+const onlyEvens= function(num){
+  let newArray= []
+  
+  for(const even of num){
+    if (even % 2 ===0){
+      newArray.push(even)
+    }
+  }return newArray
+  }
 
 
 
+  const shortNamesOnly = function(name){
+    let num= []
+    for(const short of name){
+      if(short.length < 7){
+        num.push(short)
+      }
+    }return num
+  }
 
 
+  const dNames= function(name){
+    let newArray=[]
+    for(const d of name){
+      if (d.startsWith('D')){
+        newArray.push(d)
+      }
+    }return newArray
+  }
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
